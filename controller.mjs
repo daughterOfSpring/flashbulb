894feffc56a461a69203ac8b8880afb5b714084d
daughterOfSpring <157916594+daughterOfSpring@users.noreply.github.com>
2025-02-12 10:40:06 -0600

    adding endpoints

--- controller.mjs
+++ controller.mjs
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
-import {generateImageSource, generateNumber, readFile} from './imageService.mjs'
+import asyncHandler from 'express-async-handler';
+
+import {fetchImageList, fetchRandomImage, fetchImage, uploadImage, uploadImages} from './imageService.mjs'
 
 const app = express();
 app.use(express.json())
@@ -14,12 +16,26 @@ app.listen(PORT, async () => {
     console.log(`Server listening on port ${PORT}...`);
 });
 
-app.get('/randomImage', (req, res) => {
-    generateImageSource(generateNumber());
-    res.status(200).send({filepath: readFile()})
-})
+app.get('/image/list', (asyncHandler(async (req, res) => {
+    await fetchImageList().then(data => res.status(200).send({files: data})).catch(err => console.log(err));
+})))
 
-app.get('/randomImage/:number', (req, res) => {
-    generateImageSource(req.params.number);
-    res.status(200).send({filepath: readFile()})
-})
+app.get('/image/random', asyncHandler(async (req, res) => {
+    await fetchRandomImage().then(data => res.status(200).send({url: data})).catch(err => console.log(err));
+}))
+
+app.get('/image/:fileName', (asyncHandler(async (req, res) => {
+    const fileName = req.params.fileName;
+    await fetchImage(fileName).then(data => res.status(200).send({url: data})).catch(err => console.log(err));
+})))
+
+app.post('/upload/file/:fileName', asyncHandler(async (req, res) => {
+    const fileName = req.params.fileName;
+    await uploadImage(fileName).then(data => res.status(200).send({message: data})).catch(err => console.log(err));
+}))
+
+app.get('/upload/directory/:dirName', asyncHandler(async (req, res) => {
+    const dirName = req.params.dirName;
+    await uploadImages(dirName).then(data => res.status(200).send({message: data})).catch(err => console.log(err));
+}))
+
